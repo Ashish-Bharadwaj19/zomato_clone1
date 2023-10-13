445c3f303ff594e1195f520b8d8ede7a9d6dc7de
Ashish-Bharadwaj19 <147751410+Ashish-Bharadwaj19@users.noreply.github.com>
2023-10-13 16:08:50 +0530

    z

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="absolute-center max-width">Made with 🔥 by clone makers</div>
+  );
+};
+
+export default Footer;
